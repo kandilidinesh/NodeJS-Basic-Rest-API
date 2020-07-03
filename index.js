4ae2e292bee8a775898de5e2f0dec61ca9f8679a
kandilidinesh <kandilidinesh@gmail.com>
2020-07-03 11:31:01 +0530

    Added a get req to get course object

--- index.js
+++ index.js
@@ -2,18 +2,47 @@ const express = require('express');
 
 const app = express();
 
+const courses = [
+    {
+        id: 1, name: 'Course1'
+    },
+    {
+        id: 2, name: 'Course2'
+    },
+    {
+        id: 3, name: 'Course3'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
-//Parameter in URL
+//Parameter & Queries in URL
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course){
+        return res.status(404).send('The course was not found');
+    }
+    return res.send(course);
+
 });
+
+//Create a new course
+app.post('/api/courses', (req, res) => {
+
+});
+
+
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     //res.send(req.params);
+//     res.send(req.query);
+// });
 
 //PORT 
 const port = process.env.PORT || 4000;

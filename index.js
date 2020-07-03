05686b2ebf065434c8dad87a1501522653232a1a
kandilidinesh <kandilidinesh@gmail.com>
2020-07-03 11:10:17 +0530

    Added env var for PORT and added Param in URL.

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3]);
+});
+
+//Parameter in URL
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+//PORT 
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    // console.log("Listening on port " + port.toString() +"...");
+    console.log(`Listening on port ${port}...`)
+});

98a8de60c3c0cf10bdaa8443edd78188a90968e2
kandilidinesh <kandilidinesh@gmail.com>
2020-07-03 12:49:41 +0530

    Added put method to update existing course and added validate function with obj destructuring

--- index.js
+++ index.js
@@ -37,24 +37,31 @@ app.get('/api/courses/:id', (req, res) => {
 
 });
 
-// //Create a new course
+//Create a new course
 app.post('/api/courses', (req, res) => {
 
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
+    // const schema = Joi.object({
+    //     name: Joi.string().min(3).required()
+    // });
 
-    const result = schema.validate(req.body);
-    // res.status(400).send(result.error.details[0].message);
-    // console.log(result);
+    // const result = schema.validate(req.body);
+    // // res.status(400).send(result.error.details[0].message);
+    // // console.log(result);
 
-    // if(!req.body.name || req.body.name.length < 3){
+    // // if(!req.body.name || req.body.name.length < 3){
+    // //     //400 - Bad Request
+    // //     return res.status(400).send('Course Name is not Valid');
+    // // }
+    // if(result.error){
     //     //400 - Bad Request
-    //     return res.status(400).send('Course Name is not Valid');
+    //     return res.status(400).send(result.error.details[0].message);
     // }
-    if(result.error){
+
+    //Function for validation
+    const {error} = validateCourse(req.body); // Getting result.error
+    if(error){
         //400 - Bad Request
-        return res.status(400).send(result.error.details[0].message);
+        return res.status(400).send(error.details[0].message);
     }
     const course = {
         id: courses.length + 1,
@@ -65,7 +72,41 @@ app.post('/api/courses', (req, res) => {
     res.send(course); 
 });
 
+//Update a course
+app.put('/api/courses/:id', (req, res) => {
+    //Look up the course
+    //If not existing 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
 
+    if(!course){
+        return res.status(404).send('The course was not found');
+    }
+
+    //Validate
+    // If invalid, return 400 - Bad Request
+    // const result = validateCourse(req.body);
+
+    //Object Destructuring
+    const {error} = validateCourse(req.body); // Getting result.error
+    if(error){
+        //400 - Bad Request
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Update the course
+    course.name = req.body.name;
+    // Return the updated course
+    console.log(course);
+    res.send(course);
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const result = schema.validate(course);
+    return result;
+}
 // app.get('/api/posts/:year/:month', (req, res) => {
 //     //res.send(req.params);
 //     res.send(req.query);
